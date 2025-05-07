5c62d63d34333503653a145b844db5fbc6608388
moinbaig1999 <moinb7881@gmail.com>
2025-05-07 17:51:57 +0530

    fix: update lead-statuses Entity
    - Added new field in lead-statuses Entity

--- src/entities/lead-statuses.entity.ts
+++ src/entities/lead-statuses.entity.ts
@@ -1,8 +1,12 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, OneToMany } from "typeorm";
 import Model from "./model.entity";
+import { Leads } from "./leads.entity";
 
 @Entity("lead_statuses")
 export class LeadStatuses extends Model {
   @Column({ unique: true })
   name: string;
+
+  @OneToMany(() => Leads, (lead) => lead.status)
+  leads: Leads[];
 }
