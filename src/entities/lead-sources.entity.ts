8f83658526b8d376a4dbe30ef855f13a785ed460
moinbaig1999 <moinb7881@gmail.com>
2025-05-07 17:51:10 +0530

    fix: update lead-sources Entity
    - Added new field in lead-sources Entity

--- src/entities/lead-sources.entity.ts
+++ src/entities/lead-sources.entity.ts
@@ -1,8 +1,12 @@
-import { Entity, Column } from "typeorm";
+import { Entity, Column, OneToMany } from "typeorm";
 import Model from "./model.entity";
+import { Leads } from "./leads.entity";
 
 @Entity("lead_sources")
 export class LeadSources extends Model {
   @Column({ unique: true })
   name: string;
+
+  @OneToMany(() => Leads, (lead) => lead.source)
+  leads: Leads[];
 }
