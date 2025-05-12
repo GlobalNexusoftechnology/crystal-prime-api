9a78a59e1e1821ec7f09e24b0a4162848c97f383
moinbaig1999 <moinb7881@gmail.com>
2025-05-12 11:37:24 +0530

    fix: update lead statuses Routes
    - updated lead statuses Routes

--- src/routes/lead-statuses.routes.ts
+++ src/routes/lead-statuses.routes.ts
@@ -1,34 +1,14 @@
 import express from "express";
-import {
-  createLeadStatusController,
-  getAllLeadStatusController,
-  getLeadStatusByIdController,
-  softDeleteLeadStatusController,
-  updateLeadStatusController,
-} from "../controllers/lead-statuses.controller";
+import { leadStatusController } from "../controllers/lead-statuses.controller";
 
-// Initialize Express router
 const router = express.Router();
+const controller = leadStatusController();
 
-// Route to create a new lead status
-// POST /lead-statuses/
-router.post("/", createLeadStatusController);
+router.post("/", controller.createLeadStatus);
+router.get("/", controller.getAllLeadStatuses);
+router.get("/:id", controller.getLeadStatusById);
+router.put("/:id", controller.updateLeadStatus);
+router.delete("/:id", controller.softDeleteLeadStatus);
 
-// Route to get a lead status by its ID
-// GET /lead-statuses/:id
-router.get("/:id", getLeadStatusByIdController);
-
-// Route to get all lead statuses
-// GET /lead-statuses/
-router.get("/", getAllLeadStatusController);
-
-// Route to update a lead status by ID
-// PUT /lead-statuses/:id
-router.put("/:id", updateLeadStatusController);
-
-// Route to soft delete a lead status by ID
-// DELETE /lead-statuses/:id
-router.delete("/:id", softDeleteLeadStatusController);
-
-// Export the router to be used in the main app
 export default router;
+
