1652bb433107fdf1243807a2e78424a910cc2435
moinbaig1999 <moinb7881@gmail.com>
2025-05-12 11:47:40 +0530

    fix: update lead sources Routes
    - updated lead sources Routes

--- src/routes/lead-sources.routes.ts
+++ src/routes/lead-sources.routes.ts
@@ -1,34 +1,16 @@
 import express from "express";
-import {
-  createLeadSourceController,
-  getAllLeadSourceController,
-  getLeadSourceByIdController,
-  softDeleteLeadSourceController,
-  updateLeadSourceController,
-} from "../controllers/lead-sources.controller";
+import { leadSourceController } from "../controllers/lead-sources.controller";
 
-// Initialize Express router
 const router = express.Router();
 
-// Route to create a new lead source
-// POST /lead-sources/
-router.post("/", createLeadSourceController);
+// Call the controller function to get the methods
+const controller = leadSourceController();
 
-// Route to get a lead source by its ID
-// GET /lead-sources/:id
-router.get("/:id", getLeadSourceByIdController);
+router.post("/", controller.createLeadSource);
+router.get("/", controller.getAllLeadSources);
+router.get("/:id", controller.getLeadSourceById);
+router.put("/:id", controller.updateLeadSource);
+router.delete("/:id", controller.softDeleteLeadSource);
 
-// Route to get all lead sources
-// GET /lead-sources/
-router.get("/", getAllLeadSourceController);
-
-// Route to update a lead source by ID
-// PUT /lead-sources/:id
-router.put("/:id", updateLeadSourceController);
-
-// Route to soft delete a lead source by ID
-// DELETE /lead-sources/:id
-router.delete("/:id", softDeleteLeadSourceController);
-
-// Export the router to be used in the main app
 export default router;
+
