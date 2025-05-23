1c76f8a325c19a5f3e6bd2fbb98130f79b01d663
moinbaig1999 <moinb7881@gmail.com>
2025-05-23 19:21:24 +0530

    fix: update lead attachments Routes
    - Added download Student Pdf Routes

--- src/routes/lead-attachments.routes.ts
+++ src/routes/lead-attachments.routes.ts
@@ -1,10 +1,18 @@
 import express from "express";
 import { leadAttachmentController } from "../controllers/lead-attachments.controller";
+import multer from "multer";
 
 const router = express.Router();
+const upload = multer();
 
 // Call the controller function to get the methods
 const controller = leadAttachmentController();
+
+router.post(
+  "/:leadId/upload",
+  upload.single("file"), controller.
+  downloadStudentPdf
+);
 
 router.post("/", controller.createAttachment);
 router.get("/", controller.getAllAttachments);
