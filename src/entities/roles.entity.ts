c5b1cd1d3e316ce956fedef0f610ce89bd942a9f
moinbaig1999 <moinb7881@gmail.com>
2025-05-23 19:18:25 +0530

    fix: update roles Entity
    - Remove  some code in roles Entity

--- src/entities/roles.entity.ts
+++ src/entities/roles.entity.ts
@@ -2,21 +2,14 @@ import { Entity, Column, OneToMany } from "typeorm";
 import Model from "./model.entity";
 import { User } from "./user.entity";
 
-export enum RoleName {
-  ADMIN = "Admin",
-  SALES = "Sales",
-  MANAGER = "Manager",
-}
-
 @Entity("roles")
 export class Role extends Model {
-  
+
   @Column({
-    type: "enum",
-    enum: RoleName,
+    type: "text",
     nullable: true,
   })
-  role: RoleName;
+  role: string;
 
   @Column("text", { array: true })
   permissions: string[];
