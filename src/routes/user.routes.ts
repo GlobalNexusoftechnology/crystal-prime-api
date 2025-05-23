9f8951f85996793d424a94d363649f2eef99ef55
moinbaig1999 <moinb7881@gmail.com>
2025-05-23 10:10:25 +0530

    fix: update user Routes
    - Added get All Users Handler, soft Delete User Handler in user Entity

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,17 +2,21 @@ import express from "express";
 import { deserializeUser } from "../middleware/deserializeUser";
 import { requireUser } from "../middleware/requireUser";
 import { singleUpload } from "../middleware";
-import { getProfileController, updateProfileController } from "../controllers";
+import { getAllUsersHandler, getProfileController, softDeleteUserHandler, updateProfileController } from "../controllers";
 
 const router = express.Router();
 
 // Ensure user is authenticated
 router.use(deserializeUser, requireUser);
 
-// Get user profile details
+
 router.get("/getProfile", getProfileController);
 
-// Update user profile by ID
+router.get("/", getAllUsersHandler);
+
+router.delete("/:id", softDeleteUserHandler);
+
+
 router.put("/:id", singleUpload, updateProfileController);
 
 export default router;
