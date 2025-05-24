50f622708d81ad1fdf78e7939db1ed557f570b1b
moinbaig1999 <moinb7881@gmail.com>
2025-05-24 13:41:43 +0530

    fix: update user Routes
    - Added export Users Excel Controller Routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,21 +1,17 @@
 import express from "express";
 import { deserializeUser } from "../middleware/deserializeUser";
 import { requireUser } from "../middleware/requireUser";
-import { createUserController, getAllUsersHandler, getProfileController, softDeleteUserHandler, updateProfileController } from "../controllers";
+import { createUserController, getAllUsersHandler, getProfileController, softDeleteUserHandler, updateProfileController, exportUsersExcelController } from "../controllers";
 
 const router = express.Router();
 
 // Ensure user is authenticated
 router.use(deserializeUser, requireUser);
-
+router.get("/export", exportUsersExcelController);
 router.post("/", createUserController)
 router.get("/:id", getProfileController);
-
 router.get("/", getAllUsersHandler);
-
 router.delete("/:id", softDeleteUserHandler);
-
-
 router.put("/:id", updateProfileController);
 
 export default router;
