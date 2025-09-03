d4754379ad03ea69cc1b354b2625ea367ae0757c
Aaftab Sheikh <aaftab.sheikh@purpleskyinfotech.com>
2025-09-03 23:46:58 +0530

    chore: wp port update
    wp port updated

--- config/default.ts
+++ config/default.ts
@@ -4,7 +4,7 @@ export default {
   refreshTokenExpiresIn: 1440,
   emailFrom: "developmentbyjuned@gmail.com",
   port: 5000,
-  wsPort: 5019,
+  wsPort: 5024,
   postgresConfig: {
     host: "localhost",
     port: 5432,
