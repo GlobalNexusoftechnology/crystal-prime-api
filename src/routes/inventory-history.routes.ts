63fb79001e1d6b058386854b7685638549e7f6d3
faizahmedchoudhary <ahmedfaiz868@gmail.com>
2025-12-05 16:34:19 +0530

    changes

--- src/routes/inventory-history.routes.ts
+++ src/routes/inventory-history.routes.ts
@@ -1,8 +1,10 @@
 import { createInventoryHistory, deleteInventoryHistory, getAllInventoryHistory, getInventoryHistoryById, updateInventoryHistory } from "controllers/inventory-history.controller";
 import { Router } from "express";
+import { deserializeUser, requireUser } from "middleware";
 
 
 const router = Router();
+router.use(deserializeUser, requireUser);
 
 router.post("/", createInventoryHistory);
 router.get("/", getAllInventoryHistory);
