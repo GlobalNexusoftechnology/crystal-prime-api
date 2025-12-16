ab8e56dc90176071d9243942620eb91c28d35466
faizahmedchoudhary <ahmedfaiz868@gmail.com>
2025-12-16 22:27:55 +0530

    changes

--- src/schemas/clients.schema.ts
+++ src/schemas/clients.schema.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 export const createClientSchema = z.object({
   name: z.string().min(1, "Name is required"),
   contact_number: z.string().optional(),
-  email: z.string().email("Invalid email").optional(),
+  email: z.string().optional(),
   address: z.string().optional(),
   lead_id: z.string().uuid("Invalid lead ID").optional(),
   company_name: z.string().optional(),
@@ -29,7 +29,7 @@ export const updateClientSchema = createClientSchema.partial().extend({
       id: z.string().optional(), // for updating existing details
       client_contact: z.string().min(1, "Contact is required").optional(),
       contact_person: z.string().optional(),
-      email: z.string().email("Invalid email").optional(),
+      email: z.string().optional(),
       other_contact: z.string().optional(),
       designation: z.string().optional(),
       _delete: z.boolean().optional(), // flag to delete detail
