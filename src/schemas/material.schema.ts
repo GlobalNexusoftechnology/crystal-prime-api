7129769d1db2d4bc23fc51b67248a50cb3e387d0
Faizahmedchoudhary <ahmedfaiz868@gmail.com>
2026-03-02 14:13:23 +0530

    changes

--- src/schemas/material.schema.ts
+++ src/schemas/material.schema.ts
@@ -1,15 +1,16 @@
 import { z } from "zod";
-const statePricesSchema = z.object({
-  "Maharashtra": z.number().min(0),
-  "Gujarat": z.number().min(0),
-  "Uttar_Pradesh": z.number().min(0),
-  "Karnataka": z.number().min(0),
-  "West_Bengal": z.number().min(0),
-  "Delhi": z.number().min(0),
-  "Odisha": z.number().min(0),
-  "Goa": z.number().min(0),
-});
+// const statePricesSchema = z.object({
+//   "Maharashtra": z.number().min(0),
+//   "Gujarat": z.number().min(0),
+//   "Uttar_Pradesh": z.number().min(0),
+//   "Karnataka": z.number().min(0),
+//   "West_Bengal": z.number().min(0),
+//   "Delhi": z.number().min(0),
+//   "Odisha": z.number().min(0),
+//   "Goa": z.number().min(0),
+// });
 
+const statePricesSchema = z.record(z.string(), z.number().min(0));
 
 export const createMaterialSchema = z.object({
   name: z.string().min(1, "Material name is required"),
@@ -29,8 +30,7 @@ export const createMaterialSchema = z.object({
   purchase_description: z.string().optional(),
   alias: z.string().optional(),
   quantity: z.number().min(0).optional(),
-    state_prices: statePricesSchema.optional(),
-
+  state_prices: statePricesSchema.optional(),
 });
 
-export const updateMaterialSchema = createMaterialSchema.partial(); 
+export const updateMaterialSchema = createMaterialSchema.partial();
