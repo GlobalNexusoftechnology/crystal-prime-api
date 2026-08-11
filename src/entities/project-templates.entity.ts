99bb776ace4d0f82fc43d130ec590e03498ad5dd
Faizahmedchoudhary <ahmedfaiz868@gmail.com>
2026-08-11 18:48:03 +0530

    changes

--- src/entities/project-templates.entity.ts
+++ src/entities/project-templates.entity.ts
@@ -1,21 +1,21 @@
-import { Entity, Column, OneToMany } from 'typeorm';
-import Model from './model.entity';
-import { ProjectMilestoneMaster } from './milestone-master.entity';
+import { Entity, Column, OneToMany } from "typeorm";
+import Model from "./model.entity";
+import { ProjectMilestoneMaster } from "./milestone-master.entity";
 
-@Entity('project_templates')
+@Entity("project_templates")
 export class ProjectTemplates extends Model {
-    @Column({ type: 'text' })
-    name: string;
+  @Column({ type: "text" })
+  name: string;
 
-    @Column({ type: 'text', nullable: true })
-    description: string;
+  @Column({ type: "text", nullable: true })
+  description: string;
 
-    @Column({ type: 'text', nullable: true })
-    project_type: string;
+  @Column({ type: "text", nullable: true })
+  project_type: string;
 
-    @Column({ type: 'int', nullable: true })
-    estimated_days: number;
+  @Column({ type: "int", nullable: true })
+  estimated_days?: number;
 
-    @OneToMany(() => ProjectMilestoneMaster, (milestone) => milestone.template)
-    project_milestone_master: ProjectMilestoneMaster[];
+  @OneToMany(() => ProjectMilestoneMaster, (milestone) => milestone.template)
+  project_milestone_master: ProjectMilestoneMaster[];
 }
